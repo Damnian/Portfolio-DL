513ac367fc4d737352af66133741d97656170065
Damian Lewandowski <Damian Lewandowski>
2016-08-01 20:12:56 +0200

    First commit

---
+++ js/togglebutton.js
@@ -0,0 +1,24 @@
+//przycisk menu
+var nav_container= document.querySelector('.nav_container');
+var menu = document.querySelector('.menu0');
+var menuButton = document.querySelector('.menu-toggle');
+  menuButton.addEventListener('click', function () {
+  nav_container.classList.toggle('visible');
+  menu.classList.toggle('visible');
+  menuButton.classList.toggle('closed');
+},false);
+
+
+
+//scrolling do sekcji
+$('a[href^=#]').click(function(){
+    event.preventDefault();
+    var target = $(this).attr('href');
+    if (target == '#')
+      $('html, body').animate({scrollTop : 0}, 600);
+    else
+      $('html, body').animate({
+        scrollTop: $(target).offset().top - 100
+    }, 600);
+
+});
